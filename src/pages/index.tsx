5459db97298f1ef16b60e66c29c5551472b3f71d
daniel s <iam@w-b.dev>
2021-04-21 22:44:22 -0300

    feat: add favicons and manifest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,25 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>React Avancado</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="site.webmanifest" />
       </Head>
       <main>React Avancado</main>
     </div>
